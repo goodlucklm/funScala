d0a92014685b8019c4987d8e5e75078d6362c9a7
mlu <goodlucklm@gmail.com>
2018-03-01 12:48:17 -0800

    added one more test case. did some final tweak

--- src/test/scala/com/funScala/leetcode/IntegerArraySpec.scala
+++ src/test/scala/com/funScala/leetcode/IntegerArraySpec.scala
@@ -5,8 +5,11 @@ import org.scalatest.{FunSpec, Matchers}
 class IntegerArraySpec extends FunSpec with Matchers {
   val ia  = new IntegerArrays
   describe("two sum") {
-    it("normal case") {
+    it("given [1,3,2,4,5] and 9 should return [3,4]") {
       ia.twoSum(Array(1,3,2,4,5), 9 ) shouldBe Array(3,4)
+    }
+    it("given empty arry and 8 should return []") {
+      ia.twoSum(Array(), 8 ) shouldBe Array()
     }
   }
 }
