d1d69b4e4f2968e9a19fed5242440736e14791b4
mlu <ming.lu@visier.com>
2019-01-16 19:50:54 -0800

    two sum implementation and tests

--- src/test/scala/com/funScala/leetcode/IntegerArraySpec.scala
+++ src/test/scala/com/funScala/leetcode/IntegerArraySpec.scala
@@ -1,12 +1,16 @@
 package com.funScala.leetcode
 
-import org.scalatest.{FunSpec, Matchers}
+import org.scalatest._
 
 class IntegerArraySpec extends FunSpec with Matchers {
   val ia  = new IntegerArrays
   describe("two sum") {
-    it("normal case") {
+    it("should return [3,4] given [(1,2,3,4,5), 9]") {
       ia.twoSum(Array(1,3,2,4,5), 9 ) shouldBe Array(3,4)
+    }
+
+    it("should return [0,4] given [(1,2,3,4,9), 10]") {
+      ia.twoSum(Array(1,3,2,4,9), 10 ) shouldBe Array(0,4)
     }
   }
 }
